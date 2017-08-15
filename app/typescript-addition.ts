fbc2210ff86d7aecbcf648f759ca0127a890b3a2
Guest User <Guest@epicodus-94.local>
2017-08-15 11:02:26 -0700

    practice to-do with js typescript

---
+++ app/typescript-addition.ts
@@ -0,0 +1,7 @@
+var findSum = function(firstValue: number, secondValue: number) {
+  var sum: number = firstValue + secondValue;
+  alert("The sum of your two numbers is: " + sum);
+}
+var firstNumber = parseInt(prompt('Please enter a number'));
+var secondNumber = parseInt(prompt('Please enter a number'));
+findSum(firstNumber, secondNumber);
